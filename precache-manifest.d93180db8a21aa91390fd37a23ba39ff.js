2e65b2a0db2086d9ab8c041536c318528386dec2
Oliver Fleckenstein <Oliverfl@live.dk>
2019-02-03 16:49:18 +0100

    Updates

---
+++ precache-manifest.d93180db8a21aa91390fd37a23ba39ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "03f7bf7b24753bec3fd2",
+    "url": "/GameOfLife/static/js/runtime~main.03f7bf7b.js"
+  },
+  {
+    "revision": "636ed2243196cd5c14ec",
+    "url": "/GameOfLife/static/js/main.636ed224.chunk.js"
+  },
+  {
+    "revision": "7db66ec1f1d20502f08c",
+    "url": "/GameOfLife/static/js/1.7db66ec1.chunk.js"
+  },
+  {
+    "revision": "636ed2243196cd5c14ec",
+    "url": "/GameOfLife/static/css/main.e9f064cb.chunk.css"
+  },
+  {
+    "revision": "73bf7ab551ec4eb50481f877fff9177d",
+    "url": "/GameOfLife/index.html"
+  }
+];
